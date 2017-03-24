de2b8d1b690b9783d30b8ab6c1de0dd9d226b89e
dom_linux <dom_linux@ddddd.xy>
2017-03-24 20:27:42 +0100

    client

---
+++ src/client/Value.java
@@ -0,0 +1,23 @@
+package client;
+
+public class Value {
+
+	
+	private Long id;
+	private String quote;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	
+	
+}

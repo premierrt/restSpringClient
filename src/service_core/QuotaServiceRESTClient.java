098c96b2e4fb242afe606e9b68ba42f59a29c914
dom_linux <dom_linux@ddddd.xy>
2017-04-03 00:15:19 +0200

    service_core

---
+++ src/service_core/QuotaServiceRESTClient.java
@@ -0,0 +1,22 @@
+package service_core;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import client.QuoteResponse;
+
+@Service
+public class QuotaServiceRESTClient {
+private static final Log log = LogFactory.getLog(QuotaServiceRESTClient.class);
+	
+	public void quota(){
+	
+	RestTemplate restTemplate=new RestTemplate();
+    QuoteResponse quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", QuoteResponse.class);
+    
+    log.info(quote.toString());
+    
+	}
+	
+}
